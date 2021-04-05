fd5e8d57718a2336bf2bcda5942a9220eab56ebf
zaimeali <zaimeali1997@gmail.com>
2021-04-04 22:53:52 -0400

    User Management

---
+++ README.md
@@ -0,0 +1,1 @@
+ejs package for template engine
--- server.js
+++ server.js
@@ -1,10 +1,44 @@
 // Packages
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const morgan = require("morgan");
+const path = require("path");
 
 // Initialize App
 const app = express();
 
+// Port
+const port = process.env.PORT || 8000;
+
 // Middlewares
-app.use(morgan("common"));
 app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(morgan("tiny"));
+
+// Set View Engine
+app.set("view engine", "ejs");
+// app.set("views", path.resolve(__dirname, "views"));
+
+// Load Assets
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+
+// Routes
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/add-user", (req, res) => {
+    res.render("add_user");
+});
+
+// Run Server
+app.listen(port, () => {
+    console.log(`Running on Port ${port}`);
+});
